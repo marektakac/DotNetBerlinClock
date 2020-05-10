5bdc23d366236828a07fb34410c2e358695738b1
Marek Takac <marek.takac@coresystems.ch>
2020-05-10 12:50:14 +0200

    Code improvement: Adding handling for a special case of time value '24:00:00'

--- Classes/Time.cs
+++ Classes/Time.cs
@@ -5,7 +5,16 @@
 
     internal class Time : ITime
     {
-        private const string TimePattern = "(?<hours>[0-1][0-9]|2[0-3]):(?<minutes>[0-5][0-9]):(?<seconds>[0-5][0-9])";
+        private const string TimePatternNormalCase = "(?<hours>[0-1][0-9]|2[0-3]):(?<minutes>[0-5][0-9]):(?<seconds>[0-5][0-9])";
+        private const string TimePatternSpecialCase = "(?<hours>24):(?<minutes>00):(?<seconds>00)";
+        private static string TimePattern
+        {
+            get
+            {
+                return $"{TimePatternNormalCase}|{TimePatternSpecialCase}";
+            }
+        }
+
         public int Hours { get; }
         public int Minutes { get; }
         public int Seconds { get; }
@@ -28,7 +37,7 @@
             if (!match.Success)
             {
                 throw new ArgumentOutOfRangeException(nameof(text), text,
-                    "Expected a time value in format 'HH:MM:SS'.");
+                    "Expected a time value in format 'HH:mm:ss'.");
             }
 
             int hours = int.Parse(match.Groups["hours"].Value);
